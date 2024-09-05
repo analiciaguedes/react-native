471375f325f6a85867b6b08b64d293c7c0386315
analiciaguedes <2022211510031@iesp.edu.br>
2024-09-05 13:00:39 -0300

    primeiro commit

---
+++ react-native/aula01_2/App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Exercicio02 from './src/components/Exercicio02'
+
+
+export default function App() {
+  return (
+    <View>
+     <Exercicio02/>
+    </View>
+  )
+}
